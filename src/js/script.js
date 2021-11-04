2ad1314d3763fff842a77e06585bbcef098230a1
Andrey <andrey.mahinitch@gmail.com>
2021-11-04 12:09:26 +0200

    changed hight in section consultation of @media 576 and added script.js file into repo

---
+++ src/js/script.js
@@ -0,0 +1,71 @@
+// var name="Andrey";
+
+// let number=7;
+// const pi=3.14;
+
+// let leftBorderWidth = 200;
+
+// number
+// String
+// null
+// undefined
+// {
+
+// }
+// let obj = {
+//     name: "apple",
+//     color: "green",
+//     weight: 200
+// }
+// alert(1234);
+// console.log(number);
+// let answer = confirm("Are you an adult?");
+// console.log("answer");
+
+// let answer = prompt("Are you an adult?", "");
+// console.log("answer");
+
+// console.log(4-"fgdgf");
+
+
+// let isChecked = false,
+    // isClosed=false;
+
+// console.log(isChecked && isClosed);
+// console.log(isChecked || isClosed);
+
+// if (2*6 == 8*1) {
+//     console.log ('верно')
+// } else {
+//     console.log('ошибка')
+// }
+
+// let answer = confirm("Are you an adult?");
+// console.log("answer");
+// if (answer) {
+//     console.log('Come in')
+// } else {
+//     console.log('Restricted')
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log ('Wrong')
+// } else if (num > 100) {
+//     console.log ('Too much')
+// } else {
+//     console.log ('Right')
+// }
+
+// for (let i = 1; i < 8; i++) {
+//     console.log (i);
+// }
+
+// function logging (a, b) {
+//     console.log(a + b)
+// }
+
+// logging (3 , 5);
+
+// logging (6 , 8);
