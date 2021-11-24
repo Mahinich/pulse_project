4a1e50b0791a7299b8cab57c61b68cbb7ead6fbc
Andrey <andrey.mahinitch@gmail.com>
2021-11-24 13:00:08 +0200

    changes in section caorusel. Slick slider was selected and adapted to 768 an 576 screens

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,18 @@
-// $(document).ready(function(){
-// 	$('.carousel__inner').slick({
-// 		speed: 1000,
-// 		prevArrow: '<button type="button" class="slick-prev"> <img src="icon/left.png"> </button>',
-// 		nextArrow: '<button type="button" class="slick-next"> <img src="icon/right.png"> </button>',
-// 		responsive:[
-// 			{
-// 				breakpoint: 992,
-// 				settings: {
-// 				  infinite: true,
-// 				  dots: true,
-// 				  arrows: false
-// 				}
-// 			},
-// 		]
-// 	});
-// });
+$(document).ready(function(){
+	$('.carousel__inner').slick({
+		speed: 1000,
+		prevArrow: '<button type="button" class="slick-prev"> <img src="icon/left.png"> </button>',
+		nextArrow: '<button type="button" class="slick-next"> <img src="icon/right.png"> </button>',
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+				  infinite: true,
+				  dots: true,
+				  arrows: false
+				}
+			},
+		]
+	});
+});
 
-const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-	  controls: false,
-	  nav: false
-  });
-  document.querySelector('.prev').addEventListener('click', function () {
-	slider.goTo('prev');
-  });
-
-  document.querySelector('.next').addEventListener('click', function () {
-	slider.goTo('next');
-  });
