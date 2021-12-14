28f9ed13b6e737a8d4047bec7a8e559667949dd3
Andrey <andrey.mahinitch@gmail.com>
2021-12-14 22:57:01 +0200

    validation forms are ready and script is optimized

--- src/js/script.js
+++ src/js/script.js
@@ -52,7 +52,34 @@ $(document).ready(function(){
 		});
 	});
 
-	$('#consultation-form').validate();
-	$('#consultation form').validate();
-	$('#order form').validate();
+	function validateForms(form) {
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					minlength: 2
+				  },
+				phone:"required", 
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: {
+					required: "Пожалуйста введите свое имя",
+					minlength: jQuery.validator.format("Введите {0} символов")
+				  },
+				phone: "Пожалуйста, введите свое номер",
+				email: {
+				  required: "Пожалуйста, введите свою почту",
+				  email: "Неправильно введен адрес почты"
+				}
+			  }
+		});
+	};
+
+	validateForms('#consultation-form');
+	validateForms('#consultation form');
+	validateForms('#order form');
 });
