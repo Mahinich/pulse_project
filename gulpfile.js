6bba47469ee44ecb4c279ca7cb4f3965ac187679
Andrey <andrey.mahinitch@gmail.com>
2022-01-24 08:37:24 +0200

    .js files cocantinated into single script.min.js wit gulp-uglify-es

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 const {src, dest, watch, parallel} = require('gulp');
 
-const sass = require('gulp-sass')(require('sass'));
-const concat = require('gulp-concat');
-const browserSync = require('browser-sync').create();
+const sass         = require('gulp-sass')(require('sass'));
+const concat       = require('gulp-concat');
+const browserSync  = require('browser-sync').create();
+const uglify       = require('gulp-uglify-es').default;
 const autoprefixer = require('gulp-autoprefixer');
 
 function browsersync () {
@@ -11,6 +12,20 @@ function browsersync () {
             baseDir:'src/'
         }
     })
+}
+
+function scripts() {
+    return src([
+        'src/js/slick.min.js',
+        'src/js/jquery.validate.min.js',
+        'src/js/jquery.maskedinput.js',
+        'src/js/wow.js',
+        'src/js/script.js'
+    ])
+        .pipe(concat('script.min.js'))
+        .pipe(uglify())
+        .pipe(dest('src/js'))
+        .pipe(browserSync.stream())
 }
 
 function styles () {
@@ -27,11 +42,13 @@ function styles () {
 
 function watching () {
     watch(['src/sass/**/*.sass'], styles);
+    watch(['src/js/**/*.js','!src/js/script.min.js'], scripts);
     watch('src/*.html').on ('change', browserSync.reload);
 }
 
 exports.styles = styles;
 exports.watching = watching;
 exports.browsersync = browsersync;
+exports.scripts = scripts;
 
-exports.default = parallel(browsersync, watching);
+exports.default = parallel(scripts, browsersync, watching);
